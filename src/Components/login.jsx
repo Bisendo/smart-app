cec98065b102295437c2d5e86b6e2ea76c2ebd7b
Bisendo <kimotobidaus@gmail.com>
2024-12-03 08:59:56 +0300

    new changes

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -125,7 +125,7 @@ const LoginPage = () => {
             <div>
               <button
                 type="submit"
-                className="w-full py-2 px-4 bg-green-500  text-white font-semibold rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full py-2 px-4 bg-blue-500  text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
                 Login
               </button>
@@ -150,7 +150,7 @@ const LoginPage = () => {
   <span className="text-black "> </span>
   <Link
     to="/forgot-password"
-    className="text-white bg-blue-600 rounded px-4 py-2 no-underline"
+    className="text-white bg-green-600 rounded px-4 py-2 no-underline"
   >
   Forget password?
   </Link>
